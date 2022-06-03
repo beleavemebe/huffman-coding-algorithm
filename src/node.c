c0733b2226882aa5b1b70c4f551df5e50432a031
Roman <cborzyy@gmail.com>
2022-06-03 17:58:34 +0300

    Implement node.h

---
+++ src/node.c
@@ -0,0 +1,43 @@
+//
+// Created by Roman on 03-Jun-22.
+//
+
+#include "../include/node.h"
+#include <stdlib.h>
+#include <string.h>
+
+struct node *node_create(
+    int freq,
+    char value,
+    struct node *left,
+    struct node *right
+) {
+    struct node node = {
+        .freq = freq,
+        .value = value,
+        .left = left,
+        .right = right
+    };
+
+    struct node *ptr = (struct node *) calloc(1, sizeof(struct node));
+    memcpy(ptr, &node, sizeof(struct node));
+
+    return ptr;
+}
+
+void node_destroy(struct node *node) {
+    node->freq = 0;
+    node->value = 0;
+
+    if (node->right != NULL) {
+        node_destroy(node->right);
+        free(node->right);
+        node->right = NULL;
+    }
+
+    if (node->left != NULL) {
+        node_destroy(node->left);
+        free(node->left);
+        node->left = NULL;
+    }
+}
