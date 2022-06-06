c6a3d1303b4707ebe1f3870b6a87399bed90fcc8
Roman <cborzyy@gmail.com>
2022-06-06 18:59:35 +0300

    Create freqbook.h

---
+++ include/freqbook.h
@@ -0,0 +1,20 @@
+//
+// Created by Roman on 06-Jun-22.
+//
+
+#ifndef HUFFMAN_FREQBOOK_H
+#define HUFFMAN_FREQBOOK_H
+
+#define FREQBOOK_SIZE 256
+
+struct freqbook {
+    int *frequencies;
+};
+
+struct freqbook freqbook_create();
+void freqbook_destroy(struct freqbook *freqbook);
+int freqbook_get_freq(struct freqbook *freqbook, char c);
+void freqbook_inc_freq(struct freqbook *freqbook, char c);
+struct heap freqbook_to_node_heap(struct freqbook *freqbook);
+
+#endif //HUFFMAN_FREQBOOK_H
